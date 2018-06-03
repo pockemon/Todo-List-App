be616d270eb2405ffe6869d7639919d0f8317739
Hardik <hardikrana276@gmail.com>
2018-06-04 04:16:27 +0530

    form and models added

--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 
+from . models import Todo
+
+from . forms import TodoForm
+
 # Create your views here.
 def index(request):
-    context = {}
+    todo_list = Todo.objects.order_by('id')
+
+    form = TodoForm()
+
+    context = {'todo_list': todo_list, 'form' : form}
+
     return render(request, 'todo/index.html', context)
